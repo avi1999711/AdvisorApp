96977a44bd88e10ed61622cdf9c28a4b8247fd95
Avish <avishbansal2@gmail.com>
2021-10-23 18:20:32 +0530

    testing new changes

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/avish/Challanges/NurtureLabs"
+set -gx VIRTUAL_ENV "/home/avish/Challanges/AdvisorApp"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(NurtureLabs) "
-            printf "%s%s" "(NurtureLabs) " (set_color normal)
+        if test -n "(AdvisorApp) "
+            printf "%s%s" "(AdvisorApp) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
